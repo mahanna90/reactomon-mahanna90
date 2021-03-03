979806afac81d1399336585e029adb6255da7a39
mahanna <mahanna@mahanna.com>
2021-03-03 11:46:29 +0100

    components folder added

---
+++ src/components/PokemonList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class PokemonList extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Pokemons</h1>
+            </div>
+        )
+    }
+}
+
+
