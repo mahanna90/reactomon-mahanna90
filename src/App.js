415bf501993585aecc52a465eb5bdc372158bc14
mahanna <mahanna@mahanna.com>
2021-03-03 11:47:00 +0100

    state created for testing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './App.css';
+import PokemonList from './components/PokemonList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Reactomon
-        </p>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+  state = {
+    pokemons: [
+      {
+        name: "Pikachu",
+        url: "xyz.hu",
+        id: 1
+      },
+      {
+        name: "Charmander",
+        url: "asd.hu",
+        id: 2
+      }
+    ]
+  }
+
+  render () {
+    console.log(this.state.pokemons);
+    return (
+      <div className="App">
+        <header className="App-header">
+          <PokemonList />
+        </header>
+      </div>
+    );
+  }
 }
 
-export default App;
