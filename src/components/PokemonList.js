76a5d2391b013117e1af1aed8f83f42ac5de11fb
mahanna <mahanna@mahanna.com>
2021-03-03 12:24:26 +0100

    div created for pokemon renderin with params

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react'
 
 export default class PokemonList extends Component {
     render() {
-        return (
-            <div>
-                <h1>Pokemons</h1>
+        return this.props.pokemons.map((pokemon) => (
+            <div className="card" key={ pokemon.id } >
+                <h3>{ pokemon.name }</h3>
+                <p>{ pokemon.url }</p>
             </div>
-        )
+        ))
+            
+        
     }
 }
 
