184f9c31fc316250409d4c990b9e375da85d976b
mahanna <mahanna@mahanna.com>
2021-03-06 21:26:04 +0100

    Changed Pagination component to function based structure

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,20 +1,40 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import React from 'react';
 
-export default class Pagination extends Component {
+const Pagination = ({next, previous, changePage}) => {
+
+    const src = {previous: previous, next: next};
+
+    return (
+        <div className="pagination" >
+            {previous && <button className="btn prev" onClick={changePage.bind(this, src)} >Prev</button> }
+            {next && <button className="btn next" onClick={changePage.bind(this, src)} >Next</button>} 
+        </div>
+    )
+}
+
+export default Pagination;
+
+
+// export default class Pagination extends Component {
     
 
-    render() {
+//     render() {
 
-        const src = {previous: this.props.previous, next: this.props.next};
+//         const src = {previous: this.props.previous, next: this.props.next};
 
-        return (
-            <div className="pagination" >
-                {this.props.previous ? 
-                <button className="btn prev" onClick={this.props.changePage.bind(this, src)} >Prev</button> : ""}
-                {this.props.next ?
-                <button className="btn next" onClick={this.props.changePage.bind(this, src)} >Next</button> : ""} 
-            </div>
-        )
-    }
-}
+//         return (
+//             <div className="pagination" >
+//                 {this.props.previous ? 
+//                 <button className="btn prev" onClick={this.props.changePage.bind(this, src)} >Prev</button> : ""}
+//                 {this.props.next ?
+//                 <button className="btn next" onClick={this.props.changePage.bind(this, src)} >Next</button> : ""} 
+//             </div>
+//         )
+//     }
+// }
+
+
+
+
 
