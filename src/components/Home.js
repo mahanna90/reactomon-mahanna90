99ce42469ba1636219c118c0bcf42a24f46bd9aa
mahanna <mahanna@mahanna.com>
2021-03-08 20:41:29 +0100

    Added picture and styled component to Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
+import Pikachu from '../images/byebye.png';
+
+const Image = styled.img`
+    max-width: 350px;
+`;
 
 const Home = () => {
     return (
         <>
-            {/* <div className="home" >
-                <img src={title} alt="pikachu_title_image" />
-            </div> */}
+            <Image src={Pikachu} alt="pikachu" />
+            <div className="home" >
+                
+            </div>
         </>
     )
 }
