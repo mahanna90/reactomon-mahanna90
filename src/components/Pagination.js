d51035aa760ac84a0ad073801dc8fc5649dc0ebb
mahanna <mahanna@mahanna.com>
2021-06-14 08:22:48 +0200

    delete dead code

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,4 +1,3 @@
-// import React, { Component } from 'react';
 import React from 'react';
 
 const Pagination = ({goToNextPage, goToPrevPage}) => {
@@ -12,27 +11,3 @@ const Pagination = ({goToNextPage, goToPrevPage}) => {
 }
 
 export default Pagination;
-
-
-// export default class Pagination extends Component {
-    
-
-//     render() {
-
-//         const src = {previous: this.props.previous, next: this.props.next};
-
-//         return (
-//             <div className="pagination" >
-//                 {this.props.previous ? 
-//                 <button className="btn prev" onClick={this.props.changePage.bind(this, src)} >Prev</button> : ""}
-//                 {this.props.next ?
-//                 <button className="btn next" onClick={this.props.changePage.bind(this, src)} >Next</button> : ""} 
-//             </div>
-//         )
-//     }
-// }
-
-
-
-
-
